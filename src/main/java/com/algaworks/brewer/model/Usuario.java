12c28e38bd0b8808d503e1210a7b2bc4d8be6e99
leandrocredendio <leandro.credendio@gmail.com>
2018-05-27 18:22:16 -0300

    novas implementacoes

--- src/main/java/com/algaworks/brewer/model/Usuario.java
+++ src/main/java/com/algaworks/brewer/model/Usuario.java
@@ -12,10 +12,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -25,6 +27,7 @@ import com.algaworks.brewer.validation.AtributoConfirmacao;
 				, message = "Confirmação da senha não confere")
 @Entity
 @Table(name = "tb_usuario")
+@DynamicUpdate
 public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -55,6 +58,12 @@ public class Usuario implements Serializable {
 
 	@Column(name = "data_nascimento")
 	private LocalDate dataNascimento;
+	
+	@PreUpdate
+	private void preUpdate() {
+		this.confirmacaoSenha = senha;
+	}
+	
 
 	public Long getCodigo() {
 		return codigo;
